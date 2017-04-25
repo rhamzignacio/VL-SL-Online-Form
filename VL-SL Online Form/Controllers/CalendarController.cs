1dd4074d73171c8e49bf8fb6cc9093b4638a4bd7
Royce Stephan Ignacio <ramil.charles.ignacio@gmail.com>
2017-04-25 17:57:26 +0800

    Leave form View

---
+++ VL-SL Online Form/Controllers/CalendarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VL_SL_Online_Form.Controllers
+{
+    public class CalendarController : Controller
+    {
+        // GET: Calendar
+        public ActionResult Schedule()
+        {
+            return View();
+        }
+    }
+}
