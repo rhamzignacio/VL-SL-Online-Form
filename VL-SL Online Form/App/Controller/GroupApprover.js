f6261d2cef8d8c9f58387f3c858df1ff3cce4263
Royce Stephan Ignacio <ramil.charles.ignacio@gmail.com>
2017-06-19 17:53:31 +0800

    2

--- VL-SL Online Form/App/Controller/GroupApprover.js
+++ VL-SL Online Form/App/Controller/GroupApprover.js
@@ -2,4 +2,74 @@
 
 .controller("groupApproverController", function ($scope, $location, $http, $growl) {
     var vm = this;
+
+    SuccessMessage = function (message) {
+        growl.success(message, { ttl: 2000 });
+    }
+
+    ErrorMessage = function (message) {
+        growl.error(message, { title: "Error!", ttl: 3000 });
+    }
+
+    $scope.InitGroupApprover = function(){
+        $http({
+            method: "POST",
+            url: "/GroupApprover/GetGroupApproverDropDown",
+            arguments: { "Content-Type": "application/json" }
+        }).then(function (data) {
+            if (data.data.errorMessage != "") {
+                ErrorMessage(data.data.errorMessage);
+            }
+            else {
+                vm.GroupApproverDropDown = data.data.Group;
+            }
+        })
+    }
+
+    $scope.SelectGroup = function () {
+        $http({
+            method: "POST",
+            url: "/GroupApprover/GetGroupMember",
+            data: { ID: vm.SelectedGroup }
+        }).then(function (data) {
+            if (data.data.errorMessage != "") {
+                ErrorMessage(data.data.errorMessage);
+            }
+            else {
+                vm.GroupMember = data.data.member;
+
+                $("#GroupModal").modal('hide');
+            }
+        });
+    }
+
+    $scope.SaveGroup = function(value){
+        $http({
+            method: "POST",
+            url: "/GroupApprover/SaveGroupApprover",
+            data: { group: value }
+        }).then(function(data){
+            if(data.data === ""){
+                SuccessMessage("Successfully Saved");
+            }
+            else{
+                ErrorMessage(data.data);
+            }
+        });
+    }
+
+    $scope.SaveMember = function (value) {
+        $http({
+            method: "POST",
+            url: "GroupApprover/SaveGroupMember",
+            data: { member: value }
+        }).then(function (data) {
+            if (data.data === "") {
+                SuccessMessage("Successfully Saved");
+            }
+            else {
+                ErrorMessage(data.data);
+            }
+        });
+    }
 });
--- VL-SL Online Form/Controllers/GroupApproverController.cs
+++ VL-SL Online Form/Controllers/GroupApproverController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VL_SL_Online_Form.Services;
+using VL_SL_Online_Form.Models;
 
 namespace VL_SL_Online_Form.Controllers
 {
@@ -11,6 +13,48 @@ namespace VL_SL_Online_Form.Controllers
         public ActionResult GroupApprover()
         {
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetGroupApproverDropDown()
+        {
+            string serverResponse = "";
+
+            var group = GroupApproverService.GetGroupDropDown(out serverResponse);
+
+            return Json(new { errorMessage = serverResponse, group = group });
+        }
+
+        [HttpPost]
+        public JsonResult GetGroupMember(Guid ID)
+        {
+            string serverResponse = "";
+
+            var member = GroupApproverService.GetMembers(ID, out serverResponse);
+
+            return Json(new { errorMessage = serverResponse, member = member });
+        }
+
+        [HttpPost]
+        public JsonResult SaveGroupApprover(GroupApproverModel group)
+        {
+            string serverResponse = "";
+
+            if (group != null)
+                GroupApproverService.SaveGroup(group, out serverResponse);
+
+            return Json(serverResponse);
+        }
+
+        [HttpPost]
+        public JsonResult SaveGroupMember(GroupApproverMemberModel member)
+        {
+            string serverResponse = "";
+
+            if (member != null)
+                GroupApproverService.SaveMember(member, out serverResponse);
+
+            return Json(serverResponse);
         }
     }
 }
